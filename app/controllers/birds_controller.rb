e9e25d7686479161f8ea74b1da9cd063169dd6e1
Lori Henderson <lorim95@gmail.com>
2022-07-20 13:55:39 -0400

    commit tree a01c60484b4f39fcddda6a05f15a8b857d8230a9

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -50,4 +50,16 @@ class BirdsController < ApplicationController
     params.permit(:name, :species, :likes)
   end
 
+  # DELETE destroy action
+  def destroy
+    bird = Bird.find_by(id: params[:id])
+
+    if bird
+      bird.destroy
+      head :no_content
+    else
+      render json: { error: "Bird not found" }, status: :not_found
+    end
+  end
+
 end
